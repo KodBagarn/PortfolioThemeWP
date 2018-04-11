d191fc029f00468eceb64acf9308be5c0be31400
KodBagarn <korveniglaset@gmail.com>
2018-04-11 16:04:31 +0200

    a

--- portfolio-theme-KodBagarn-master/portfolio-theme-KodBagarn-master/header.php
+++ portfolio-theme-KodBagarn-master/portfolio-theme-KodBagarn-master/header.php
@@ -1,7 +1,8 @@
   <?php get_template_part('partials/sitehead');  ?>
 <body>
   <header>
-    <h1 id="hometitle">  <a href="<?php  echo home_url();?>"> <?php bloginfo('name');  ?> </a> </h1>
+    <p>this is a header</p>
+    <img src="img/My logo (for white background).png" alt="logo">  <a href="<?php  echo home_url();?>"> <?php bloginfo('name');  ?> </a> </h1> </img>
     <?php //get_sidebar(); ?>
     <?php wp_nav_menu(array('theme_location' => 'header-nav')); ?>
 
--- portfolio-theme-KodBagarn-master/portfolio-theme-KodBagarn-master/monsters inc/media.php
+++ portfolio-theme-KodBagarn-master/portfolio-theme-KodBagarn-master/monsters inc/media.php
@@ -1,4 +1,4 @@
 <?php
-add_image_size( 'grid_thumbnail', 300, 300, true );
+add_image_size( 'grid_thumbnail', 500, 500, true );
 add_image_size( 'single_large', 660, 400, false );
 ?>
